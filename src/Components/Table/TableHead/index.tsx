5928195b4ccbf203a5701916b279cba2c026ef16
kawa2019 <kamil@silentridge.io>
2022-10-22 23:12:23 +0200

    adding default sort

--- src/Components/Table/TableHead/index.tsx
+++ src/Components/Table/TableHead/index.tsx
@@ -1,60 +1,64 @@
-import { FC, useState } from 'react';
-import { DebtApiKey } from '../../../Services/Api/interfaces';
+import {FC, useEffect, useState} from 'react';
+import {DebtApiKey} from '../../../Services/Api/interfaces';
 import './index.sass';
 import OrderIcon from '../../OrderIcon';
-import { Order } from '../../../Services/Table/interfaces';
+import {Order} from '../../../Services/Table/interfaces';
 
 interface TableHeadProps {
-  handleSorting: (sortField: DebtApiKey, sortOrder: Order) => void;
+    handleSorting: (sortField: DebtApiKey, sortOrder: Order) => void;
 }
 
-const TableHead: FC<TableHeadProps> = ({ handleSorting }) => {
-  const [sortField, setSortField] = useState<DebtApiKey>('Name');
-  const [order, setOrder] = useState<Order>('asc');
+const TableHead: FC<TableHeadProps> = ({handleSorting}) => {
+    const [sortField, setSortField] = useState<DebtApiKey>('Name');
+    const [order, setOrder] = useState<Order>('asc');
 
-  const handleSortingChange = (accessor: DebtApiKey) => {
-    const sortOrder = accessor === sortField && order === 'asc' ? 'desc' : 'asc';
-    setSortField(accessor);
-    setOrder(sortOrder);
-    handleSorting(accessor, sortOrder);
-  };
+    useEffect(() => {
+        handleSorting(sortField, order);
+    }, [])
 
-  return (
-    <thead className={'TableHead'}>
-      <tr>
-        <th
-          className={'TableHead-Th TableHead-Th--name'}
-          onClick={() => handleSortingChange('Name')}>
-          <div className={'TableHead-ColumnWrapper'}>
-            <p className={'TableHead-ColumnName'}>Dłużnik</p>
-            <OrderIcon sortField={sortField} field={'Name'} order={order} />
-          </div>
-        </th>
-        <th className={'TableHead-Th TableHead-Th--nip'} onClick={() => handleSortingChange('NIP')}>
-          <div className={'TableHead-ColumnWrapper'}>
-            <p className={'TableHead-ColumnName'}>NIP</p>
-            <OrderIcon sortField={sortField} field={'NIP'} order={order} />
-          </div>
-        </th>
-        <th
-          className={'TableHead-Th TableHead-Th--value'}
-          onClick={() => handleSortingChange('Value')}>
-          <div className={'TableHead-ColumnWrapper'}>
-            <p className={'TableHead-ColumnName'}>Kwota zadłużenia</p>
-            <OrderIcon sortField={sortField} field={'Value'} order={order} />
-          </div>
-        </th>
-        <th
-          className={'TableHead-Th TableHead-Th--date'}
-          onClick={() => handleSortingChange('Date')}>
-          <div className={'TableHead-ColumnWrapper'}>
-            <p className={'TableHead-ColumnName'}>Data powstania zobowiązania</p>
-            <OrderIcon sortField={sortField} field={'Date'} order={order} />
-          </div>
-        </th>
-      </tr>
-    </thead>
-  );
+    const handleSortingChange = (accessor: DebtApiKey) => {
+        const sortOrder = accessor === sortField && order === 'asc' ? 'desc' : 'asc';
+        setSortField(accessor);
+        setOrder(sortOrder);
+        handleSorting(accessor, sortOrder);
+    };
+
+    return (
+        <thead className={'TableHead'}>
+        <tr>
+            <th
+                className={'TableHead-Th TableHead-Th--name'}
+                onClick={() => handleSortingChange('Name')}>
+                <div className={'TableHead-ColumnWrapper'}>
+                    <p className={'TableHead-ColumnName'}>Dłużnik</p>
+                    <OrderIcon sortField={sortField} field={'Name'} order={order}/>
+                </div>
+            </th>
+            <th className={'TableHead-Th TableHead-Th--nip'} onClick={() => handleSortingChange('NIP')}>
+                <div className={'TableHead-ColumnWrapper'}>
+                    <p className={'TableHead-ColumnName'}>NIP</p>
+                    <OrderIcon sortField={sortField} field={'NIP'} order={order}/>
+                </div>
+            </th>
+            <th
+                className={'TableHead-Th TableHead-Th--value'}
+                onClick={() => handleSortingChange('Value')}>
+                <div className={'TableHead-ColumnWrapper'}>
+                    <p className={'TableHead-ColumnName'}>Kwota zadłużenia</p>
+                    <OrderIcon sortField={sortField} field={'Value'} order={order}/>
+                </div>
+            </th>
+            <th
+                className={'TableHead-Th TableHead-Th--date'}
+                onClick={() => handleSortingChange('Date')}>
+                <div className={'TableHead-ColumnWrapper'}>
+                    <p className={'TableHead-ColumnName'}>Data powstania zobowiązania</p>
+                    <OrderIcon sortField={sortField} field={'Date'} order={order}/>
+                </div>
+            </th>
+        </tr>
+        </thead>
+    );
 };
 
 export default TableHead;
